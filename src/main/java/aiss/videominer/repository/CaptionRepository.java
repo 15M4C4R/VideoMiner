043c32ef6709033fe24b3bcf77a5c72c7e7f5141
15M4C4R <mkhazni2004@gmail.com>
2024-05-11 12:45:50 +0200

    Repository done

--- src/main/java/aiss/videominer/repository/CaptionRepository.java
+++ src/main/java/aiss/videominer/repository/CaptionRepository.java
@@ -1,9 +1,9 @@
 package aiss.videominer.repository;
 
-import aiss.videominer.model.Channel;
+import aiss.videominer.model.Caption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChannelRepository extends JpaRepository<Channel, String> {
+public interface CaptionRepository extends JpaRepository<Caption, String> {
 }
